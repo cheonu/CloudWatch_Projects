bda4f026aadb89704745937065961f34017e7dc5
chedev <cheonu85@gmail.com>
2020-06-23 17:54:27 -0400

    initial commit

--- Describe_CloudWatch_Alarms.py
+++ Describe_CloudWatch_Alarms.py
@@ -7,3 +7,4 @@ cloudwatch=boto3.client("cloudwatch")
 paginator=cloudwatch.get_paginator("describe_alarms")
 for response in paginator.paginate(StateValue='INSUFFICIENT_DATA'):
     print(response["MetricAlarms"])
+#new
