140990edef13d4b6e5a92f19ea3f8167b58de185
chedev <cheonu85@gmail.com>
2020-06-24 11:26:45 -0400

    put_metric code

---
+++ Create_CloudWatch_Alarms.py
@@ -0,0 +1,22 @@
+import boto3
+
+cloudwatch=boto3.client("cloudwatch")
+
+cloudwatch.put_metric_alarm(
+    AlarmName='chedev2',
+    AlarmDescription='Alarm when CPU utilization exceeds 70%',
+    ActionsEnabled=False,
+    MetricName='CPUUtilization',
+    Namespace='AWS/EC2',
+    Statistic='Average',
+    EvaluationPeriods=1,
+    Threshold=70.0,
+    ComparisonOperator='GreaterThanThreshold',
+    Dimensions=[
+        {
+          'Name': 'InstanceId',
+          'Value': 'INSTANCE_ID'
+        },
+    ],
+    Unit='Seconds'
+)
